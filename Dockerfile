4d35b4691b34e7c8ba67979c2e82f03e1816fc7e
Timm Friebe <thekid@thekid.de>
2020-12-09 10:56:51 +0100

    Initial release

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM debian:stretch
+
+RUN apt-get -y update && apt-get install -y \
+    python3-pip \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN useradd -ms /bin/bash app
+
+USER app
+
+WORKDIR /home/app
+
+RUN pip3 install pysummarize
+
+RUN python3 -c 'import nltk; nltk.download(["stopwords", "punkt"])'
+
+COPY src /home/app/src
+
+CMD ["python3", "/home/app/src/serve.py"]
---
+++ src/serve.py
@@ -0,0 +1,54 @@
+import os
+import cgi
+import re
+import html
+import sys
+
+from summarize import summarize
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Summarize(BaseHTTPRequestHandler):
+  def output(self, template, context):
+    def replace(match):
+      return html.escape(str(context[match.group(1)]))
+
+    self.wfile.write(re.sub('\{\{([^}]+)\}\}', replace, self.templates[template]).encode())
+
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.send_header('Connection', 'close')
+    self.end_headers()
+
+    self.output('form', {})
+
+  def do_POST(self):
+    form = cgi.FieldStorage(fp=self.rfile, headers=self.headers, environ={'REQUEST_METHOD': 'POST'})
+    text = form.getvalue('text')
+    result = summarize(text, sentence_count=3, language=form.getvalue('language'))
+
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.send_header('Connection', 'close')
+    self.end_headers()
+
+    self.output('result', {'text': text, 'result': result, 'sentences': 3})
+
+if __name__ == '__main__':
+  handler = Summarize
+  handler.templates = {}
+
+  path = os.path.dirname(sys.argv[0])
+  for file in os.listdir(path):
+    if file.endswith('.html'):
+      with open(os.path.join(path, file), 'r') as f:
+        handler.templates[os.path.basename(file)[0 : -5]] = f.read()
+
+  try:
+      port = 8080
+      server = HTTPServer(('', port), handler)
+      print("Web Server running on port %s" % port)
+      server.serve_forever()
+  except KeyboardInterrupt:
+      print("stopping web server....")
+      server.socket.close()
